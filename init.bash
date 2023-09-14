89e51d0b43da9a0c7ca71984538c5f4b79cf9767
Steve Jensen <steve@cleo.cloud>
2023-09-14 05:25:34 -0500

    skip tests on sub module

--- init.bash
+++ init.bash
@@ -4,10 +4,10 @@
 git submodule update --init
 
 # Build Up serialization
-mvn install -f LambdaEventsV4/aws-lambda-java-serialization/pom.xml
+mvn install -f LambdaEventsV4/aws-lambda-java-serialization/pom.xml -DskipTests
 
 # Build Up Java Events V4
-mvn install -f LambdaEventsV4/aws-lambda-java-events/pom.xml
+mvn install -f LambdaEventsV4/aws-lambda-java-events/pom.xml -DskipTests
 
 # and finally parent pom for the lambdas
 mvn install
