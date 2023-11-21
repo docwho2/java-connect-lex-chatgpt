8d4da9134e86b2accfe04e033b04042a5679aae9
Steve Jensen <docwho2@gmail.com>
2023-11-21 04:37:08 -0600

    POM Cleanup

--- init.bash
+++ init.bash
@@ -3,11 +3,5 @@
 # ensure sub modules are brought in
 git submodule update --init
 
-# Build Up serialization
-mvn install -f LambdaEventsV4/aws-lambda-java-serialization/pom.xml -DskipTests
-
-# Build Up Java Events V4
-mvn install -f LambdaEventsV4/aws-lambda-java-events/pom.xml -DskipTests
-
 # and finally parent pom for the lambdas
-mvn install
+mvn install -DskipTests
